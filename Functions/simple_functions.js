947c85e5b9f62a4ceadb01be28d6a3968a17d306
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 14:21:30 -0400

    completed excercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function favoriteThings(){
+    let thename = "Megan";
+    let movie = " Red Notice";
+    let music = " J Cole";
+console.log(thename);
+console.log (movie);
+console.log(music);
+}
+
+
+function whyImHere() {
+    let display = "I've joined the learn to code progoram because the track interested me, I have many ideas I Would like to bring to life";
+   console.log(display);
+}
+
+function favoritePlace() { 
+    let place = "Trinidad";
+    console.log ("My favorite place to visit "  + place);
+
+}
+favoriteThings();
+whyImHere();
+favoritePlace();
---
+++ Functions/whyimhere.js
@@ -0,0 +1,5 @@
+function whyImHere(){
+    let explain = " I've joined the learn to code progoram because the track interested me, I have many ideas I Would like to bring to life";
+    console.log = (explain);
+}
+whyImHere();
