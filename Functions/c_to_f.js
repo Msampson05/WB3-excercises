204a45884cd91c696a7ed3ae52feff8d3dc4592d
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 17:56:27 -0400

    competed f to c

--- Functions/c_to_f.js
+++ Functions/c_to_f.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// Celsius to Fahrenheit 
+
+function convertCtoF(currentTemp, fahrenhietTemp){
+let convert = (currentTemp*9/5) +32;
+console.log(convert);
+}
+let theCurrentTemp = 40;
+let thefahrenhietTemp = ((theCurrentTemp * 9/5)+ 32);
+console.log("The celsius " + theCurrentTemp + " converts to " + thefahrenhietTemp + " farenhiet Safter converted ");
+
+convertCtoF();
