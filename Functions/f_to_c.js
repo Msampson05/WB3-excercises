2c6352404bb1b0fab967d1d1bfa20c3d20d11357
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 17:44:54 -0400

    added c to f file

--- Functions/f_to_c.js
+++ Functions/f_to_c.js
@@ -4,7 +4,7 @@ function convertFtoC(currentTemp , celsiusTemp){
 let convert = ((currentTemp-32)* 5/9);
 console.log(convert)
 }
-let theCurrentTemp = 92;
+let theCurrentTemp = 40;
 let theCelsiusTemp = ((theCurrentTemp - 32)* 5/9);
 console.log("The fahrenhiet " + theCurrentTemp + " converts to " + theCelsiusTemp + " after converted ");
 
