519ea2bb8ef595f9738c3c556ba11ea57c829643
Megan Sampson <msampson@nyc.yearup.org>
2022-10-25 09:24:23 -0400

    completed socsectax

--- Functions/tax_functions.js
+++ Functions/tax_functions.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function getSocSecTax(){
-
+function getSocSecTax(grossPay,taxRate ){
+  let SocSecTax = (grossPay*taxRate);
+  console.log(SocSecTax);
 }
-
+let thegrossPay = 70000;
+let thetaxRate = (6/100);
+let theSocSecTax= (thegrossPay * thetaxRate);
+console.log("With a gross pay of " + thegrossPay + " and a tax rate of 6.2%, Your social security will be " + theSocSecTax )
