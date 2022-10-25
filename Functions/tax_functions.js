6a3207bcb70fd5d7cd3a6ecfb4e89f44120e387b
Megan Sampson <msampson@nyc.yearup.org>
2022-10-25 10:05:25 -0400

    finished tax functions

--- Functions/tax_functions.js
+++ Functions/tax_functions.js
@@ -28,6 +28,42 @@ console.log("With a gross pay of " + theGrossPay + " and a medicare tax of 1.45%
 
 "use strict";
 
-function  getFederalTax(){
-    
+function  getFederalTax(fedGrossPay,withholdCode){
+  let Federaltax = (fedGrossPay * withholdCode);
+  console.log(Federaltax)
 }
+//Person 1 
+let theFedGrossPay = 750;
+let thewithHoldCode1 = (23/100);
+let theFederalTax = (theFedGrossPay * thewithHoldCode1);
+console.log("     ")
+console.log("Person 1")
+console.log("With a gross pay of " + theFedGrossPay + " and a withholding code of 0, Your federal tax is " + theFederalTax );
+
+"use strict";
+
+function  getFederalTax(fedGrossPay,withholdCode){
+  let Federaltax = (fedGrossPay * withholdCode);
+  console.log(Federaltax)
+}
+//Person 2 
+let theFedGrossPay2 = 1550;
+let thewithHoldCode2 = (19.5/100);
+let theFederalTax2 = (theFedGrossPay2 * thewithHoldCode2);
+console.log("     ")
+console.log("Person 2")
+console.log("With a gross pay of " + theFedGrossPay2 + " and a withholding code of 2, Your federal tax is " + theFederalTax2 );
+
+"use strict";
+
+function  getFederalTax(fedGrossPay,withholdCode){
+  let Federaltax = (fedGrossPay * withholdCode);
+  console.log(Federaltax)
+}
+//Person 3
+let theFedGrossPay3 = 1100;
+let thewithHoldCode3 = (17/100);
+let theFederalTax3 = (theFedGrossPay3 * thewithHoldCode3);
+console.log("     ")
+console.log("Person 3")
+console.log("With a gross pay of " + theFedGrossPay3 + " and a withholding code of 6, Your federal tax is " + theFederalTax3 );
