5960d412eb8ff9fd3cdc57302abd0be6dc25f3f7
Megan Sampson <msampson@nyc.yearup.org>
2022-10-26 14:34:19 -0400

    completed more dates excercise

---
+++ Dates/DateScripts/date_to_string.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let d = new Date();
+console.log(d.toString()) ;
+
+
+
+let d1= new Date();
+console.log(d1.toDateString()) ;
+
+
+
+let d2 = new Date();
+console.log(d2.toUTCString()) ;
+
+
+let d3 = new Date();
+console.log(d3.toLocaleDateString());
+console.log(d3.toLocaleTimeString());
+console.log(d3.toLocaleString());
---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,17 @@
+"use strict";
+//Create date objects for important days in my life 
+
+// Your birthday using the form "MM/DD/YYYY"
+let date1 = new Date("06/21/1998");
+//06/21/1998
+console.log(date1.toLocaleString());
+
+
+//Your mom's birthday using the form "MM DD YYYY"
+let date2 = new Date("2 , 26 ,1976");
+// 02 26 1976
+console.log(date2.toLocaleString());
+
+// You dad's birthday using new Date(yyyy, m , d)
+let date3 = new Date("1967 , 5 , 23");
+console.log(date3.toLocaleString());
